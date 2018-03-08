a2f1ebc5e6388b474aa3d239c1eead14c8e8ba4f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-08 09:46:21 +0700

    wireup gfw template to route

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 
 	jen "github.com/dave/jennifer/jen"
 	"github.com/julienschmidt/httprouter"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
 	router := httprouter.New()
-	router.GET("/", generate)
+	router.GET("/", index)
+	router.GET("/generate", generate)
 	http.ListenAndServe(":3001", router)
+}
+
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "gfw.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func generate(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
