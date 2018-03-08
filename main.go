d7001290def09168c59b101eba4e7361a81db61d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-08 09:35:03 +0700

    test basic generate go code

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	jen "github.com/dave/jennifer/jen"
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
-	fmt.Println("Hello")
+	router := httprouter.New()
+	router.GET("/", generate)
+	http.ListenAndServe(":3001", router)
+}
+
+func generate(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	f := jen.NewFile("main")
+	f.Func().Id("main").Params().Block(
+		jen.Qual("fmt", "Println").Call(jen.Lit("Hello, world")),
+	)
+	io.WriteString(w, fmt.Sprintf("%#v", f))
 }
