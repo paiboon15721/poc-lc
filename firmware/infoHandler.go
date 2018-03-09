1568576525a23217cfcb6113b02dc82b5392aa37
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2018-03-09 10:22:14 +0700

    refactor

--- firmware/infoHandler.go
+++ firmware/infoHandler.go
@@ -7,20 +7,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type quota struct {
-	Total  int `json:"total"`
-	Remain int `json:"remain"`
-}
-
-type info struct {
-	Version    string `json:"version"`
-	HardwareID string `json:"hardwareID"`
-	Customer   string `json:"customer"`
-	Quota      quota  `json:"quota"`
-}
-
 func infoHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	info := info{"0.0.1", hardwareID, customer, quota{quotaTotal, quotaTotal}}
+	type quota struct {
+		Total  int `json:"total"`
+		Remain int `json:"remain"`
+	}
+
+	type info struct {
+		Version    string `json:"version"`
+		HardwareID string `json:"hardwareID"`
+		Customer   string `json:"customer"`
+		Quota      quota  `json:"quota"`
+	}
+
+	i := info{"0.0.1", hardwareID, customer, quota{quotaTotal, quotaTotal}}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(info)
+	json.NewEncoder(w).Encode(i)
 }
