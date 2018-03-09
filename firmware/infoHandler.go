6b27150fd5a9986e05b15e431637bd77e2cc4036
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2018-03-09 09:51:17 +0700

    setup info struct

--- firmware/infoHandler.go
+++ firmware/infoHandler.go
@@ -6,6 +6,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type quota struct {
+	Total  int `json:"total"`
+	Remain int `json:"remain"`
+}
+
+type information struct {
+	Version    string `json:"version"`
+	HardwareID string `json:"hardwareID"`
+	Customer   string `json:"customer"`
+	Quota      quota  `json:"quota"`
+}
+
 func info(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 
 }
