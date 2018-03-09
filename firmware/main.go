8692808f34d0642448aa4bcd0b6982fe0d5c47ba
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-09 16:18:11 +0700

    initial boltdb before start server

--- firmware/main.go
+++ firmware/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	bolt "github.com/coreos/bbolt"
 	"github.com/julienschmidt/httprouter"
 )
 
+var db *bolt.DB
+
 func main() {
+	var err error
+	db, err = bolt.Open("lc.db", 0644, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("ids"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
 	router := httprouter.New()
 	router.GET("/info", infoHandler)
 	router.POST("/activate", activateHandler)
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3001", router)
 }
