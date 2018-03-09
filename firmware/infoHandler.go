fb6df2d793dbc68f40e54947c4d96eaa6928d84c
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2018-03-09 10:13:48 +0700

    fix wording

--- firmware/infoHandler.go
+++ firmware/infoHandler.go
@@ -12,15 +12,15 @@ type quota struct {
 	Remain int `json:"remain"`
 }
 
-type information struct {
+type info struct {
 	Version    string `json:"version"`
 	HardwareID string `json:"hardwareID"`
 	Customer   string `json:"customer"`
 	Quota      quota  `json:"quota"`
 }
 
-func info(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	info := information{"0.0.1", hardwareID, customer, quota{quotaTotal, quotaTotal}}
+func infoHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	info := info{"0.0.1", hardwareID, customer, quota{quotaTotal, quotaTotal}}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(info)
 }
