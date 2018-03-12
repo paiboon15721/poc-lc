426e0500a0e373a29b57da3a8b49e780a721e929
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-12 08:07:54 +0700

    initial ssh to linux server

--- installHandler.go
+++ installHandler.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"golang.org/x/crypto/ssh"
 )
 
 func installHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	io.WriteString(w, "test")
+	sshConfig := &ssh.ClientConfig{
+		User: "student",
+		Auth: []ssh.AuthMethod{ssh.Password("student")},
+	}
+	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+	client, _ := ssh.Dial("tcp", "157.179.132.136:22", sshConfig)
+	session, _ := client.NewSession()
+	var b bytes.Buffer
+	session.Stdout = &b
+	session.Run("ls -l")
+	io.WriteString(w, b.String())
 }
