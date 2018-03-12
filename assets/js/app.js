21d0d84f7e4e8fd2cc9776e671162f9de35cb0ee
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-12 13:02:35 +0700

    initial js to install

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
-alert("test");
+$('#btn-submit').click(function() {
+  var serverIP = $('#serverIP').val();
+  var serverUsername = $('#serverUsername').val();
+  var serverPassword = $('#serverPassword').val();
+  var customer = $('#customer').val();
+  var quotaTotal = $('#quotaTotal').val();
+  if (
+    !(serverIP && serverUsername && serverPassword && customer && quotaTotal)
+  ) {
+    alert('Required all field!');
+    return;
+  }
+  $.post(
+    '/install',
+    {
+      serverIP: serverIP,
+      serverUsername: serverUsername,
+      serverPassword: serverPassword,
+      customer: customer,
+      quotaTotal: quotaTotal
+    },
+    function(data) {
+      alert(data);
+    }
+  );
+});
