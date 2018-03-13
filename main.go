3dc5a05e5f05015f11bd24f83a5461e14a81086e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-13 16:36:16 +0700

    wireup scanIP and fix route bug

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	router := httprouter.New()
-	router.ServeFiles("/*filepath", http.Dir("assets"))
+	router.ServeFiles("/app/*filepath", http.Dir("assets"))
 	router.POST("/api/install", installHandler)
+	router.GET("/api/scan-ip", scanIPHandler)
 	http.ListenAndServe(":3001", router)
 }
