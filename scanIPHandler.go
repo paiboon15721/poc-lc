b278847021b5db56ad8031a4aed567cf6c443acf
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-13 16:56:53 +0700

    implement loop ip

--- scanIPHandler.go
+++ scanIPHandler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -9,16 +10,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// func incIP(ip net.IP) {
-// 	for j := len(ip) - 1; j >= 0; j-- {
-// 		ip[j]++
-// 		if ip[j] > 0 {
-// 			break
-// 		}
-// 	}
-// }
-
 func scanIPHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	// Get localIP
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		http.Error(w, err.Error(), 500)
@@ -26,5 +19,19 @@ func scanIPHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params
 	defer conn.Close()
 	localAddr := conn.LocalAddr().(*net.UDPAddr).String()
 	localIP := localAddr[:strings.IndexByte(localAddr, ':')]
+
+	// Scan /24 subnet
+	incIP := func(ip net.IP) {
+		for j := len(ip) - 1; j >= 0; j-- {
+			ip[j]++
+			if ip[j] > 0 {
+				break
+			}
+		}
+	}
+	ip, ipNet, _ := net.ParseCIDR(fmt.Sprintf("%s/24", localIP))
+	for ip := ip.Mask(ipNet.Mask); ipNet.Contains(ip); incIP(ip) {
+		fmt.Println(ip.String())
+	}
 	io.WriteString(w, localIP)
 }
