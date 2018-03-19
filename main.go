6526ec2cc18350d5ef0975b774af0b2edb698813
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-19 10:15:17 +0700

    wireup shutdown route

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	router := httprouter.New()
 	router.ServeFiles("/app/*filepath", http.Dir("assets"))
 	router.POST("/api/install", installHandler)
+	router.POST("/api/shutdown", shutdownHandler)
 	router.GET("/api/scan-ip", scanIPHandler)
 	router.GET("/api/get-info/:ip", getInfoHandler)
 	router.GET("/api/get-connection/:ip/:username/:password", getConnectionHandler)
