2933590f228efac78d1430762804539ff497dcb9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-03-19 14:11:18 +0700

    refactor

--- shutdownHandler.go
+++ shutdownHandler.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
-	"golang.org/x/crypto/ssh"
 )
 
 func shutdownHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -16,26 +14,18 @@ func shutdownHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Para
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	var err error
 	serverIP := req.PostFormValue("serverIP")
 	serverUsername := req.PostFormValue("serverUsername")
 	serverPassword := req.PostFormValue("serverPassword")
-	var client *ssh.Client
-	var session *ssh.Session
-	sshConfig := &ssh.ClientConfig{
-		User:    serverUsername,
-		Auth:    []ssh.AuthMethod{ssh.Password(serverPassword)},
-		Timeout: time.Duration(time.Millisecond * 2000),
-	}
-	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-	client, err = ssh.Dial("tcp", fmt.Sprintf("%s:22", serverIP), sshConfig)
+
+	client, err := getSSHClient(serverIP, serverUsername, serverPassword)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	// Shutdown server
-	session, _ = client.NewSession()
+	session, _ := client.NewSession()
 	var b bytes.Buffer
 	session.Stdout = &b
 	session.Run(fmt.Sprintf("echo \"%s\" | sudo -S poweroff", serverPassword))
